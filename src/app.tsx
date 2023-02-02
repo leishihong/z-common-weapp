3f28b34dfb72541a507c2383a2b98e5489f32583
beichen <leishihong@jd.com>
2023-02-02 17:47:51 +0800

    init

--- src/app.tsx
+++ src/app.tsx
@@ -14,6 +14,7 @@ const App: FC<PropsWithChildren> = ({ children }) => {
 
 	Taro.onPageNotFound((options) => {
 		console.log('on page not found', options);
+
 		Taro.redirectTo({
 			url: URL['404']
 		});
